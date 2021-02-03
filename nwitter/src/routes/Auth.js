82d8d6563d30d29fe097919fa58a286154c44549
Sonwanseo <sonwanseo@gmail.com>
2021-02-03 11:52:43 +0900

    #2.3 Creating Account

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,16 +1,29 @@
 import React, { useState } from 'react';
+import { authService } from 'fbase';
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccout] = useState(true);
 
     const onChange = (e) => {
         const { target: { name, value } } = e;
         if (name === "email") setEmail(value);
         else if (name === "password") setPassword(value);
     };
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault();
+        try {
+            let data;
+            if (newAccount) {
+                data = await authService.createUserWithEmailAndPassword(email, password);
+            } else {
+                data = await authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data);
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     return (
@@ -18,7 +31,7 @@ const Auth = () => {
             <form onSubmit={onSubmit}>
                 <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
                 <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
-                <input type="submit" value="Log In" />
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
             </form>
             <div>
                 <button>Continue with Google</button>
